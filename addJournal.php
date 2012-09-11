4cc54dc80482b0867e8b846b9f4c70ff72399b65
CarlAmbroselli <social@carl-ambroselli.de>
2012-09-11 15:35:58 +0200

    add journal html

---
+++ addJournal.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+  </head>
+  <body>
+   <form action="api/addJournal.php" method="post">
+   Title: <input type="text" name="title"><br>
+   Description: <input type="text" name="Description"><br>
+   Who you where with: (fbids, comma seperated) <input type="text" name="With"><br>
+   Picture url: <input type="text" name="Picture">
+   
+   </form>
+  </body>
+</html>
+   
